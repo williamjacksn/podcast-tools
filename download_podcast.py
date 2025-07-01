2882f78a7995916794a3017593775a0e9b01ed31
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-07-01 11:18:36 -0500

    ruff format

--- download_podcast.py
+++ download_podcast.py
@@ -7,30 +7,30 @@ import xml.etree.ElementTree
 
 
 def main():
-    podcast_url = os.getenv('PODCAST_URL')
+    podcast_url = os.getenv("PODCAST_URL")
     if podcast_url is None:
-        print('Please set PODCAST_URL')
+        print("Please set PODCAST_URL")
         sys.exit(1)
-    print(f'PODCAST_URL is {podcast_url}')
+    print(f"PODCAST_URL is {podcast_url}")
     response = httpx.get(podcast_url)
-    print(f'Response encoding is {response.encoding}')
+    print(f"Response encoding is {response.encoding}")
     text = response.text
     root = xml.etree.ElementTree.fromstring(text)
-    channel = root.find('channel')
-    items = channel.findall('item')
+    channel = root.find("channel")
+    items = channel.findall("item")
     working_dir = pathlib.Path().resolve()
     for item in items:
-        enclosure = item.find('enclosure')
-        url = enclosure.get('url')
+        enclosure = item.find("enclosure")
+        url = enclosure.get("url")
         parsed_url = urllib.parse.urlparse(url)
-        print(f'Downloading from: {url}')
+        print(f"Downloading from: {url}")
         source_path = pathlib.Path(parsed_url.path)
         target_path = working_dir / source_path.name
-        print(f'Saving to {target_path}')
-        with target_path.open('wb') as f:
+        print(f"Saving to {target_path}")
+        with target_path.open("wb") as f:
             source = httpx.get(url)
             f.write(source.content)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- make_podcast.py
+++ make_podcast.py
@@ -13,80 +13,93 @@ from typing import Dict, Union
 
 def get_pub_date(index: int) -> str:
     date = datetime.date(2016, 1, 1) + datetime.timedelta(days=index)
-    return f'{date:%a}, {date.day} {date:%b %Y} 08:00:00 +0000'
+    return f"{date:%a}, {date.day} {date:%b %Y} 08:00:00 +0000"
 
 
 def make_podcast_xml(podcast_info: Dict) -> ETree.Element:
-    rss_attrib = {'version': '2.0', 'xmlns:atom': 'http://www.w3.org/2005/Atom',
-                  'xmlns:itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd'}
-    rss = ETree.Element('rss', attrib=rss_attrib)
-    rss.text = '\n  '
-    rss.tail = '\n'
-    channel = ETree.SubElement(rss, 'channel')
-    channel.text = '\n    '
-    channel.tail = '\n'
-    base_url = podcast_info['base_url']
-    link_self = ETree.SubElement(channel, 'atom:link', href=f'{base_url}_.xml', rel='self')
-    link_self.tail = '\n    '
-    language = ETree.SubElement(channel, 'language')
-    language.text = 'en-us'
-    language.tail = '\n    '
-    title = ETree.SubElement(channel, 'title')
-    title.text = podcast_info['title']
-    title.tail = '\n    '
-    author = ETree.SubElement(channel, 'itunes:author')
-    author.text = podcast_info['author']
-    author.tail = '\n    '
-    image = ETree.SubElement(channel, 'itunes:image', href=f'{base_url}_.jpg')
-    image.tail = '\n    '
-    link = ETree.SubElement(channel, 'link')
-    link.text = podcast_info['base_url']
-    link.tail = '\n    '
-    description = ETree.SubElement(channel, 'description')
-    description.text = podcast_info['description']
-    description.tail = '\n    '
-    category = ETree.SubElement(channel, 'itunes:category', text='Arts')
-    category.text = '\n      '
-    category.tail = '\n    '
-    sub_category = ETree.SubElement(category, 'itunes:category', text='Literature')
-    sub_category.tail = '\n    '
-    owner = ETree.SubElement(channel, 'itunes:owner')
-    owner.text = '\n      '
-    owner.tail = '\n    '
-    email = ETree.SubElement(owner, 'itunes:email')
-    email.text = podcast_info['owner_email']
-    email.tail = '\n    '
-    explicit = ETree.SubElement(channel, 'itunes:explicit')
-    explicit.text = 'no'
-    explicit.tail = '\n    '
-    block = ETree.SubElement(channel, 'itunes:block')
-    block.text = 'yes'
-    block.tail = '\n    '
+    rss_attrib = {
+        "version": "2.0",
+        "xmlns:atom": "http://www.w3.org/2005/Atom",
+        "xmlns:itunes": "http://www.itunes.com/dtds/podcast-1.0.dtd",
+    }
+    rss = ETree.Element("rss", attrib=rss_attrib)
+    rss.text = "\n  "
+    rss.tail = "\n"
+    channel = ETree.SubElement(rss, "channel")
+    channel.text = "\n    "
+    channel.tail = "\n"
+    base_url = podcast_info["base_url"]
+    link_self = ETree.SubElement(
+        channel, "atom:link", href=f"{base_url}_.xml", rel="self"
+    )
+    link_self.tail = "\n    "
+    language = ETree.SubElement(channel, "language")
+    language.text = "en-us"
+    language.tail = "\n    "
+    title = ETree.SubElement(channel, "title")
+    title.text = podcast_info["title"]
+    title.tail = "\n    "
+    author = ETree.SubElement(channel, "itunes:author")
+    author.text = podcast_info["author"]
+    author.tail = "\n    "
+    image = ETree.SubElement(channel, "itunes:image", href=f"{base_url}_.jpg")
+    image.tail = "\n    "
+    link = ETree.SubElement(channel, "link")
+    link.text = podcast_info["base_url"]
+    link.tail = "\n    "
+    description = ETree.SubElement(channel, "description")
+    description.text = podcast_info["description"]
+    description.tail = "\n    "
+    category = ETree.SubElement(channel, "itunes:category", text="Arts")
+    category.text = "\n      "
+    category.tail = "\n    "
+    sub_category = ETree.SubElement(category, "itunes:category", text="Literature")
+    sub_category.tail = "\n    "
+    owner = ETree.SubElement(channel, "itunes:owner")
+    owner.text = "\n      "
+    owner.tail = "\n    "
+    email = ETree.SubElement(owner, "itunes:email")
+    email.text = podcast_info["owner_email"]
+    email.tail = "\n    "
+    explicit = ETree.SubElement(channel, "itunes:explicit")
+    explicit.text = "no"
+    explicit.tail = "\n    "
+    block = ETree.SubElement(channel, "itunes:block")
+    block.text = "yes"
+    block.tail = "\n    "
     return rss
 
 
-def add_item_to_podcast(rss: ETree.Element, podcast_info: Dict, mp3_info: Dict, index: int):
-    channel = rss.find('channel')
-    item = ETree.SubElement(channel, 'item')
-    item.text = '\n      '
-    item.tail = '\n    '
-    title = ETree.SubElement(item, 'title')
-    title.text = mp3_info['title']
-    title.tail = '\n      '
-    base_url = podcast_info['base_url']
-    item_name = mp3_info['name']
-    length = str(mp3_info['size'])
-    enclosure = ETree.SubElement(item, 'enclosure', url=f'{base_url}{item_name}', type='audio/mpeg', length=length)
-    enclosure.tail = '\n      '
-    guid = ETree.SubElement(item, 'guid', isPermaLink='false')
+def add_item_to_podcast(
+    rss: ETree.Element, podcast_info: Dict, mp3_info: Dict, index: int
+):
+    channel = rss.find("channel")
+    item = ETree.SubElement(channel, "item")
+    item.text = "\n      "
+    item.tail = "\n    "
+    title = ETree.SubElement(item, "title")
+    title.text = mp3_info["title"]
+    title.tail = "\n      "
+    base_url = podcast_info["base_url"]
+    item_name = mp3_info["name"]
+    length = str(mp3_info["size"])
+    enclosure = ETree.SubElement(
+        item,
+        "enclosure",
+        url=f"{base_url}{item_name}",
+        type="audio/mpeg",
+        length=length,
+    )
+    enclosure.tail = "\n      "
+    guid = ETree.SubElement(item, "guid", isPermaLink="false")
     guid.text = str(uuid.uuid4())
-    guid.tail = '\n      '
-    pub_date = ETree.SubElement(item, 'pubDate')
+    guid.tail = "\n      "
+    pub_date = ETree.SubElement(item, "pubDate")
     pub_date.text = get_pub_date(index)
-    pub_date.tail = '\n      '
-    duration = ETree.SubElement(item, 'itunes:duration')
-    duration.text = mp3_info['duration']
-    duration.tail = '\n    '
+    pub_date.tail = "\n      "
+    duration = ETree.SubElement(item, "itunes:duration")
+    duration.text = mp3_info["duration"]
+    duration.tail = "\n    "
 
 
 def duration_string(num_seconds: int) -> str:
@@ -101,7 +114,7 @@ def duration_string(num_seconds: int) -> str:
         else:
             hours = minutes // 60
             minutes %= 60
-    return f'{hours}:{minutes:02d}:{seconds:02d}'
+    return f"{hours}:{minutes:02d}:{seconds:02d}"
 
 
 def get_mp3s(path: Union[pathlib.Path, str]):
@@ -111,39 +124,55 @@ def get_mp3s(path: Union[pathlib.Path, str]):
         p = pathlib.Path(path).resolve()
     if p.is_dir():
         for item in p.iterdir():
-            if item.suffix.lower() == '.mp3':
+            if item.suffix.lower() == ".mp3":
                 yield item
 
 
 def get_file_info(mp3: pathlib.Path):
-    info = {'name': mp3.name}
+    info = {"name": mp3.name}
     audio = mutagen.mp3.MP3(str(mp3))
-    info['duration'] = duration_string(int(audio.info.length))
-    info['size'] = mp3.stat().st_size
+    info["duration"] = duration_string(int(audio.info.length))
+    info["size"] = mp3.stat().st_size
     id3 = mutagen.id3.ID3(str(mp3))
-    info['title'] = id3.getall('TIT2')[0][0]
+    info["title"] = id3.getall("TIT2")[0][0]
     return info
 
 
 def main():
-    source_dir = os.getenv('SOURCE_DIR')
+    source_dir = os.getenv("SOURCE_DIR")
     if source_dir is None:
-        print('Please set SOURCE_DIR to the directory containing your mp3 files')
+        print("Please set SOURCE_DIR to the directory containing your mp3 files")
         sys.exit(1)
-    print(f'Source directory is {source_dir}')
+    print(f"Source directory is {source_dir}")
     path = pathlib.Path(source_dir).resolve()
-    print(f'Resolved source directory is {path}')
-    info = {'base_url': '', 'title': '', 'author': '', 'description': '', 'owner_email': ''}
-    saved_info_path = path / 'podcast_info.json'
+    print(f"Resolved source directory is {path}")
+    info = {
+        "base_url": "",
+        "title": "",
+        "author": "",
+        "description": "",
+        "owner_email": "",
+    }
+    saved_info_path = path / "podcast_info.json"
     if saved_info_path.exists():
         with saved_info_path.open() as f:
             info = json.load(f)
-    info['base_url'] = input('Base url [{base_url}]: '.format(**info)) or info['base_url']
-    info['title'] = input('Podcast title [{title}]: '.format(**info)) or info['title']
-    info['author'] = input('Podcast author [{author}]: '.format(**info)) or info['author']
-    info['description'] = input('Podcast description [{description}]: '.format(**info)) or info['description']
-    info['owner_email'] = input('Podcast owner email [{owner_email}]: '.format(**info)) or info['owner_email']
-    with saved_info_path.open('w') as f:
+    info["base_url"] = (
+        input("Base url [{base_url}]: ".format(**info)) or info["base_url"]
+    )
+    info["title"] = input("Podcast title [{title}]: ".format(**info)) or info["title"]
+    info["author"] = (
+        input("Podcast author [{author}]: ".format(**info)) or info["author"]
+    )
+    info["description"] = (
+        input("Podcast description [{description}]: ".format(**info))
+        or info["description"]
+    )
+    info["owner_email"] = (
+        input("Podcast owner email [{owner_email}]: ".format(**info))
+        or info["owner_email"]
+    )
+    with saved_info_path.open("w") as f:
         json.dump(info, f, indent=2)
 
     rss = make_podcast_xml(info)
@@ -152,8 +181,8 @@ def main():
         add_item_to_podcast(rss, info, get_file_info(mp3), index)
 
     tree = ETree.ElementTree(rss)
-    tree.write(path / '_.xml', encoding='utf-8', xml_declaration=True)
+    tree.write(path / "_.xml", encoding="utf-8", xml_declaration=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
