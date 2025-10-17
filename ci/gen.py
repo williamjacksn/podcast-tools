aff85b4a22cd185440ec759e8e94606fe0fec12d
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-10-17 14:14:50 -0500

    Change dependabot interval from daily to weekly

--- ci/gen.py
+++ ci/gen.py
@@ -2,7 +2,33 @@ import json
 import pathlib
 
 
-def gen(content: dict, target: str):
+def gen(content: dict, target: str) -> None:
+    pathlib.Path(target).parent.mkdir(parents=True, exist_ok=True)
     pathlib.Path(target).write_text(
         json.dumps(content, indent=2, sort_keys=True), newline="\n"
     )
+
+
+def gen_dependabot() -> None:
+    target = ".github/dependabot.yaml"
+    content = {
+        "version": 2,
+        "updates": [
+            {
+                "package-ecosystem": e,
+                "allow": [{"dependency-type": "all"}],
+                "directory": "/",
+                "schedule": {"interval": "weekly"},
+            }
+            for e in ["docker", "github-actions", "uv"]
+        ],
+    }
+    gen(content, target)
+
+
+def main() -> None:
+    gen_dependabot()
+
+
+if __name__ == "__main__":
+    main()
